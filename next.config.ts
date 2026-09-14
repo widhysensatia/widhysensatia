de7c55dbff1acdbe9ee047d25ec2b5b2344a9778
widhy sensatia <widhysensatia@gmail.com>
2026-09-14 11:46:36 +0800

    Remove output: export to allow PM2/next start

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["127.0.0.1"],
-  output: "export",
+
   trailingSlash: true,
   images: {
     unoptimized: true,
